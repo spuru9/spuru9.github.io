3dc7f50a379e2c4df28eb3305387e61a8d94fffb
Purushottam Sinha <puru@Purushottams-MacBook-Air.local>
2026-04-10 23:18:31 +0530

    Enhance UI with glassmorphism, animations, and refined typography

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import BlogsPage from './BlogsPage';
 import AboutMe from './AboutMe';
+import BookReviews from './BookReviews';
 import './App.css';
 
 function App() {
@@ -12,23 +13,27 @@ function App() {
           <div className="nav-logo">Purushottam Sinha</div>
           <ul className="nav-links">
             <li>
-              <Link to="/">Home (Blogs)</Link>
+              <Link to="/">About Me</Link>
             </li>
             <li>
-              <Link to="/about">About Me</Link>
+              <Link to="/technical">Technical</Link>
+            </li>
+            <li>
+              <Link to="/book-reviews">Book Reviews</Link>
             </li>
           </ul>
         </nav>
 
         <main className="content">
           <Routes>
-            <Route path="/" element={<BlogsPage />} />
-            <Route path="/about" element={<AboutMe />} />
+            <Route path="/" element={<AboutMe />} />
+            <Route path="/technical" element={<BlogsPage />} />
+            <Route path="/book-reviews" element={<BookReviews />} />
           </Routes>
         </main>
 
         <footer className="footer">
-          <p>&copy; 2026 My Portfolio. All rights reserved.</p>
+          <p>&copy; 2026 Purushottam Sinha. All rights reserved.</p>
         </footer>
       </div>
     </Router>
